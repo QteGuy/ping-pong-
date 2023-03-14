8e57fe0c9872fffc3cc9effbd147bd4fe2d8c1c9
QteGuy <127236503+QteGuy@users.noreply.github.com>
2023-03-14 19:35:17 +0100

    Updated data.py

--- data.py
+++ data.py
@@ -1,9 +1,13 @@
-import pygame
-SPR_SIZE = (10,100)
-WND_SIZE = (1080, 720)
+import pygame as pygame
+SPR_SIZE = (10, 100)
+WND_SIZE = (1920, 1080)
+FPS = 60
+window = pygame.display.set_mode(WND_SIZE)
+background = pygame.transform.scale(pygame.image.load("pingpong_bg.jpeg"), WND_SIZE)
+clock = pygame.time.Clock()
 
 class GameSprite(pygame.sprite.Sprite):
-    def __init__(self, image_name, speed, pos_x, pos_y):
+    def __init__(self, image_name, pos_x, pos_y, speed):
         super().__init__()
         self.image = pygame.transform.scale(pygame.image.load(image_name), SPR_SIZE)
         self.speed = speed
@@ -12,20 +16,21 @@ class GameSprite(pygame.sprite.Sprite):
         self.rect.y = pos_y
 
     def reset(self):
-        pygame.window.blit(self.image, (self.rect.x, self.rect.y))
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player(GameSprite):
+class Player1(GameSprite):
     def update_l(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w] and self.rect.y > 10:
             self.rect.y -= self.speed
-        if keys[pygame.K_s]:
+        if keys[pygame.K_s] and self.rect.y < 730:
             self.rect.y += self.speed
 
+class Player2(GameSprite):
     def update_r(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_UP] and self.rect.y > 10:
             self.rect.y -= self.speed
-        if keys[pygame.K_DOWN]:
+        if keys[pygame.K_DOWN] and self.rect.y < 730:
             self.rect.y += self.speed
 
