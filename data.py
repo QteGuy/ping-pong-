c01b174cfdd2db54feee76549c0a74ed5fad2a6c
QteGuy <127236503+QteGuy@users.noreply.github.com>
2023-03-28 17:58:50 +0200

    Wrong update LOL!

--- data.py
+++ data.py
@@ -1,37 +1,40 @@
-from data import *
+import pygame
+SPR_SIZE = (10, 100)
+BL_SIZE = (50,50)
+WND_SIZE = (1920, 720)
+FPS = 60
+GREEN = (50, 255, 50)
+window = pygame.display.set_mode(WND_SIZE)
+background = pygame.transform.scale(pygame.image.load("pingpong_bg.jpeg"), WND_SIZE)
+clock = pygame.time.Clock()
+pygame.font.init()
+font_label = pygame.font.SysFont("Comic Sans MS", 32)
 
-player1 = Player1("player1.png", 10, 10, 5, SPR_SIZE)
-player2 = Player2("player2.png", 1420, 10, 5, SPR_SIZE)
-ball = GameSprite("ball.png", 560, 560, 5, BL_SIZE)
-speed_x = 5
-speed_y = 5
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, image_name, pos_x, pos_y, speed, size):
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load(image_name), size)
+        self.speed = speed
+        self.rect = self.image.get_rect()
+        self.rect.x = pos_x
+        self.rect.y = pos_y
+        self.size = size
 
-win1_label = font_label.render("Player 1 wins!", 1, GREEN)
-win2_label = font_label.render("Player 2 wins!", 1, GREEN)
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
-game = True
-game_over = False
-while game:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            game = False
-    if not game_over:
-        window.blit(background, (0, 0))
-        ball.reset()
-        player1.reset()
-        player2.reset()
-        ball.rect.x += speed_x
-        ball.rect.y += speed_y
-        if ball.rect.y >= WND_SIZE[1] - BL_SIZE[1] or ball.rect.y <= 0:
-            speed_y *= -1.1
-        if pygame.sprite.collide_rect(ball, player1) or pygame.sprite.collide_rect(ball, player2):
-            speed_x *= -1.1
-        if ball.rect.x <= -50:
-            window.blit(win2_label, (640, 360))
-        if ball.rect.x >= 1470:
-            window.blit(win1_label, (640, 360))
-        player1.update_l()
-        player2.update_r()
+class Player1(GameSprite):
+    def update_l(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] and self.rect.y > 10:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s] and self.rect.y < 730:
+            self.rect.y += self.speed
 
-    clock.tick(FPS)
-    pygame.display.update()
+class Player2(GameSprite):
+    def update_r(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and self.rect.y > 10:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN] and self.rect.y < 730:
+            self.rect.y += self.speed
